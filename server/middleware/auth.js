f97a081991003acf2f6d3609fb7b0f0268c82cb5
snrp2002 <snrp2002@gmail.com>
2023-04-03 22:20:27 +0530

    Update JWT_SECRET

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const auth = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
-        const tokenData = jwt.verify(token, 'test');
+        const tokenData = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = tokenData._id;
         next();
     }catch(error){
